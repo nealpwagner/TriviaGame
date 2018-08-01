88a33bc300657cc2786798931b5e107e5aec9c84
nealpwagner <nwagner22@yahoo.com>
2018-07-31 23:08:33 -0400

    added html and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,43 +6,43 @@ $(document)
             $("#quiz").hide();
 
             var choices = [];
-            choices[0] = makeChoice("teacher", 0);
-            choices[1] = makeChoice("santa", 1);
-            choices[2] = makeChoice("mother", 2);
+            choices[0] = makeChoice("Earth", 0);
+            choices[1] = makeChoice("Mercury", 1);
+            choices[2] = makeChoice("venus", 2);
 
             var question = {};
-            question.question = "\"You'll shoot your eye out, kid\"";
+            question.question = "What is the closest planet to the Sun?";
             question.choices = choices;
             question.answerIndex = 1;
 
             var choices2 = [];
-            choices2[0] = makeChoice("bloodsport", 0);
-            choices2[1] = makeChoice("Rambo", 1);
-            choices2[2] = makeChoice("terminator", 2);
+            choices2[0] = makeChoice("Neptune", 0);
+            choices2[1] = makeChoice("Jupiter", 1);
+            choices2[2] = makeChoice("Saturn", 2);
 
             var question2 = {};
-            question2.question = "\"I'll be back\"";
+            question2.question = "What is the name of the 2nd biggest planet in our solar system?";
             question2.choices = choices2;
             question2.answerIndex = 2;
 
             var choices3 = [];
-            choices3[0] = makeChoice("Ted Bundy", 0);
-            choices3[1] = makeChoice("Buffulo Bill", 1);
-            choices3[2] = makeChoice("Jeffery Dahmer", 2);
+            choices3[0] = makeChoice("Mars", 0);
+            choices3[1] = makeChoice("Venus", 1);
+            choices3[2] = makeChoice("Mercury", 2);
 
             var question3 = {};
-            question3.question = "\"It puts the lotion on its skin\"";
+            question3.question = "What is the hottest planet in our solar system?";
             question3.choices = choices3;
             question3.answerIndex = 1;
 
 
             var choices4 = [];
-            choices4[0] = makeChoice("Billy the Kid", 0);
-            choices4[1] = makeChoice("wyatt Earp", 1);
-            choices4[2] = makeChoice("Doc Holiday", 2);
+            choices4[0] = makeChoice("Jupiter", 0);
+            choices4[1] = makeChoice("Saturn", 1);
+            choices4[2] = makeChoice("Mars", 2);
 
             var question4 = {};
-            question4.question = "\"I'll make ya famous\"";
+            question4.question = "What planet is famous for its big red spot on it?";
             question4.choices = choices4;
             question4.answerIndex = 0;
 
@@ -54,8 +54,6 @@ $(document)
                 $("#question_" + (i + 1)).text(question.question);
                 for (var k = 0; k < question.choices.length; k++) {
                     var answer = question.choices[k].answer;
-                    //question-1-choice-1-label-1
-                    //question-4-choice-1-label-1
                     $("#question-" + (i + 1) + "-choice-" + (k + 1) + "-label-" + (k + 1)).text(answer);
 
                 }
@@ -63,7 +61,7 @@ $(document)
             resetScreen();
         }
     );
-    
+
 
 $("#submit").click(function () {
     processSubmit();
@@ -78,10 +76,13 @@ $("#playAgain").click(function () {
     resetScreen();
 });
 
-function resetScreen(){
+function resetScreen() {
     $("#startGame").show();
     $("#endGame").hide();
     $("#quiz").hide();
+    $("input[type=radio]").attr('disabled', false);
+    $("input[type=radio]").prop('checked', false);
+    $("#submit").prop('disabled', false);
 }
 
 function makeChoice(answer, id) {
@@ -90,15 +91,15 @@ function makeChoice(answer, id) {
     choice.id = id;
     return choice;
 }
+
 function processReset() {
     $("#quiz").show();
     $("#startGame").hide();
     $("#endGame").hide();
-    //reset timer
+    clearInterval(timer);                   
     startTimer();
-    //clear out radio button selection and enable
     $("input[type=radio]").attr('disabled', false);
-    $("input[type=radio]").prop('checked',false);
+    $("input[type=radio]").prop('checked', false);
     $("#submit").prop('disabled', false);
 }
 
@@ -109,7 +110,6 @@ function processSubmit() {
 
     //stop timer
     clearInterval(timer);
-    //disable radios
     $("input[type=radio]").attr('disabled', true);
     $("#submit").attr("disabled", "disabled");
     $("#endGame").show();
@@ -125,20 +125,25 @@ function processSubmit() {
                 if (question.answerIndex == k) {
                     correctAnswer = correctAnswer + 1;
                 }
+                $("#correctAnswer").text("Correct Guesses: " + correctAnswer);
             }
+            
         }
+        
     }
-    //alert(correctAnswer);
+    
 }
+//alert(correctAnswer);
+
 function startTimer(){
-    var start = new Date;
-    secondsLeft = 0;
-    var secondsLimit = 10;
-    timer = setInterval(function () {
-        var secondsLeft = secondsLimit - Math.round((new Date - start) / 1000);
-        $('#clock').text(secondsLeft + " Seconds Left");
-        if(secondsLeft <= 0){
-            processSubmit();
-        }
-    }, 1000);
+var start = new Date;
+secondsLeft = 0;
+var secondsLimit = 30;
+timer = setInterval(function () {
+    var secondsLeft = secondsLimit - Math.round((new Date - start) / 1000);
+    $('#clock').text(secondsLeft + " Seconds Left");
+ if(secondsLeft <= 0){
+ processSubmit();
+}
+ }, 1000);
 }
